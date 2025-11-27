b30cf31e515566ead1828adbd09af4ecbb189bfe
Josh Dang <69253231+JoshPDang@users.noreply.github.com>
2025-11-27 23:53:17 +1100

    Create gis branch, setup folders and placeholders (#35)
    * create gis branch, setup folders and placeholders
    
    update pyproject.toml for GIS
    
    update gitignore
    
    * update readme
    
    * Remove .DS_Store
    
    * Add testing dependencies for pytest
    
    ---------
    
    Co-authored-by: Lee Copland <lee@leecopland.com>

---
+++ datascience/gis/config/settings.py
@@ -0,0 +1,5 @@
+import os
+
+# Load GEE service account settings (placeholders)
+SERVICE_ACCOUNT = os.getenv("")
+KEY_PATH = os.getenv("")
---
+++ datascience/gis/core/extract_data.py
@@ -0,0 +1,23 @@
+def get_rainfall():
+    """Fetch rainfall data (placeholder)."""
+    pass
+
+
+def get_temperature():
+    """Fetch temperature data (placeholder)."""
+    pass
+
+
+def get_ph():
+    """Fetch soil pH data (placeholder)."""
+    pass
+
+
+def get_elevation():
+    """Fetch elevation data (placeholder)."""
+    pass
+
+
+def get_landcover():
+    """Fetch landcover data (placeholder)."""
+    pass
